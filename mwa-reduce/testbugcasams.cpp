caf893ac412826512304ad1284d5b236cf15da66
André Offringa <offringa@gmail.com>
2013-04-02 10:02:52 +1100

    Added bug involving virt dest

---
+++ mwa-reduce/testbugcasams.cpp
@@ -0,0 +1,17 @@
+#include <ms/MeasurementSets/MeasurementSet.h>
+
+using namespace casa;
+
+int main(int argc, char *argv[])
+{
+	// Create a MeasurementSet and cast it to a Table pointer
+	// Table is the rootclass of MeasurementSet
+	Table *ms = new MeasurementSet(argv[1]);
+	
+	// Delete the measurement set. This will only call ~Table(),
+	// and not ~MeasurementSet(), because ~Table() is non-virtual.
+	delete ms;
+	
+	// This leaks a lot of stuff!
+	return 0;
+}
