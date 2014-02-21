0ea90689418b10ae99f0afa5dd58298ae8a375b3
André Offringa <offringa@gmail.com>
2014-02-21 17:31:24 +1100

    Fixing index error in pbcorrect

--- mwa-reduce/pbcorrect.cpp
+++ mwa-reduce/pbcorrect.cpp
@@ -30,8 +30,8 @@ int main(int argc, char *argv[])
 		
 	const char
 		*inpFilename[4] = { argv[1], argv[2], argv[3], argv[4] },
-		*beamFilename[8] = { argv[7], argv[8], argv[9], argv[10], argv[11], argv[12], argv[13], argv[14] },
-		*outFilename[4] = { argv[15], argv[16], argv[17], argv[18] };
+		*beamFilename[8] = { argv[5], argv[6], argv[7], argv[8], argv[9], argv[10], argv[11], argv[12] },
+		*outFilename[4] = { argv[13], argv[14], argv[15], argv[16] };
 	
 	FitsReader firstImage(argv[1]);
 	std::vector<double> inputData[4], beamData[8];
