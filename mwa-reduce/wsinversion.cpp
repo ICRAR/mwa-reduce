a387d169b27f9cbadffa2a1dcdc6c97ba46c1a80
André Offringa <offringa@gmail.com>
2014-02-25 10:38:17 +1100

    Fixed another cfitsio crash issue

--- mwa-reduce/wsinversion.cpp
+++ mwa-reduce/wsinversion.cpp
@@ -25,9 +25,8 @@ WSInversion::MSData::MSData() : matchingRows(0), totalRowsProcessed(0)
 WSInversion::MSData::~MSData()
 { }
 
-WSInversion::WSInversion(ImageBufferAllocator<double>* imageAllocator, double memFraction) : InversionAlgorithm(), _phaseCentreDL(0.0), _phaseCentreDM(0.0), _denormalPhaseCentre(false), _hasFrequencies(false), _freqHigh(0.0), _freqLow(0.0), _bandStart(0.0), _bandEnd(0.0), _beamSize(0.0), _startTime(0.0), _gridMode(LayeredImager::NearestNeighbour), _laneBufferSize(16), _imageBufferAllocator(imageAllocator)
+WSInversion::WSInversion(ImageBufferAllocator<double>* imageAllocator, double memFraction) : InversionAlgorithm(), _phaseCentreRA(0.0), _phaseCentreDec(0.0), _phaseCentreDL(0.0), _phaseCentreDM(0.0), _denormalPhaseCentre(false), _hasFrequencies(false), _freqHigh(0.0), _freqLow(0.0), _bandStart(0.0), _bandEnd(0.0), _beamSize(0.0), _totalWeight(0.0), _startTime(0.0), _gridMode(LayeredImager::NearestNeighbour), _cpuCount(sysconf(_SC_NPROCESSORS_ONLN)), _laneBufferSize(16), _imageBufferAllocator(imageAllocator)
 {
-	_cpuCount = sysconf(_SC_NPROCESSORS_ONLN);
 	long int pageCount = sysconf(_SC_PHYS_PAGES), pageSize = sysconf(_SC_PAGE_SIZE);
 	_memSize = (int64_t) pageCount * (int64_t) pageSize;
 	double memSizeInGB = (double) _memSize / (1024.0*1024.0*1024.0);
