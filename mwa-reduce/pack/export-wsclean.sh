ac2b996d89600615a35f13a23a37a9d633b50cb4
André Offringa <offringa@gmail.com>
2014-03-31 13:18:53 +1100

    Export WSClean

---
+++ mwa-reduce/pack/export-wsclean.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+if [[ $1 == "" ]] ; then
+		echo Syntax: $0 \<dir\>
+else
+		WORKDIR=`pwd`
+		DEST="$1"
+		cd ..
+		cp -v areaset.* banddata.* beamevaluator.* buffered_lane.* cachedimageset.* fftresampler.* fitsreader.* fitswriter.* imagecoordinates.* imagebufferallocator.* imageweights.* inversionalgorithm.* lane.* layeredimager.* multibanddata.* nlplfitter.* matrix2x2.* model.* modelrenderer.* modelsource.* msselection.* polarizationenum.* progressbar.* radeccoord.* sourcesdf.* sourcesdfwithsamples.* spectralenergydistribution.* stopwatch.* tilebeam.* uvector.* uvwdistribution.* weightmode.* wsclean.* wscleanmain.cpp wsinversion.* ${DEST}/
+		cp -v aocommon/lane.h aocommon/lane_03.h aocommon/lane_11.h aocommon/uvector.h ${DEST}/aocommon/
+		cp -v cleanalgorithms/*.{h,cpp} ${DEST}/cleanalgorithms/
+		cp -v msprovider/*.{h,cpp} ${DEST}/msprovider/
+		cp -v parser/*.h ${DEST}/parser/		
+fi
