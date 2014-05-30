123c611c9e94cfa7f07e16253d49ac1583b9251d
André Offringa <offringa@gmail.com>
2014-05-30 20:17:20 +1000

    Making sure uvector_03.h is packed too

--- mwa-reduce/pack/export-wsclean.sh
+++ mwa-reduce/pack/export-wsclean.sh
@@ -6,7 +6,7 @@ else
 		DEST="$1"
 		cd ..
 		cp -v areaset.* banddata.* beamevaluator.* buffered_lane.* cachedimageset.* fftconvolver.* fftresampler.* fitsreader.* fitswriter.* imagecoordinates.* imagebufferallocator.* imageweights.* inversionalgorithm.* lane.* layeredimager.* multibanddata.* nlplfitter.* matrix2x2.* model.* modelrenderer.* modelsource.* msselection.* polarizationenum.* progressbar.* radeccoord.* sourcesdf.* sourcesdfwithsamples.* spectralenergydistribution.* stopwatch.* uvector.* uvwdistribution.* weightmode.* wsclean.* wscleanmain.cpp wsinversion.* ${DEST}/
-		cp -v aocommon/lane.h aocommon/lane_03.h aocommon/lane_11.h aocommon/uvector.h ${DEST}/aocommon/
+		cp -v aocommon/lane.h aocommon/lane_03.h aocommon/lane_11.h aocommon/uvector.h aocommon/uvector_03.h ${DEST}/aocommon/
 		cp -v beam/lnaimpedance.* beam/tilebeam*.{h,cpp} beam/tileimpedance.{h,cpp} ${DEST}/beam/
 		cp -v cleanalgorithms/*.{h,cpp} ${DEST}/cleanalgorithms/
 		cp -v msprovider/*.{h,cpp} ${DEST}/msprovider/
