d2bd62b3933e96b0ca34cc3d560e46d95db4174c
André Offringa <offringa@gmail.com>
2014-11-06 12:04:54 +0100

    Adding option to test RMS

--- mwa-reduce/imgstats.cpp
+++ mwa-reduce/imgstats.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <cmath>
 
-void reportValues(const ao::uvector<double>& samples)
+void reportValues(const ao::uvector<double>& samples, bool useLimit=false, double limitFlux=0.0)
 {
 	double rms = 0.0;
 	double avg = 0.0;
@@ -25,44 +25,84 @@ void reportValues(const ao::uvector<double>& samples)
 	}
 	stddev = sqrt(stddev / samples.size());
 	
-	std::cout << "N=" << samples.size() << "\tAvg=" << avg << "\tRMS=" << rms << "\tStddev=" << stddev << "\n";
+	if(useLimit)
+	{
+		if(rms > limitFlux)
+			std::cout << "1\n";
+	}
+	else
+		std::cout << "N=" << samples.size() << "\tAvg=" << avg << "\tRMS=" << rms << "\tStddev=" << stddev << "\n";
 }
 
 int main(int argc, char* argv[])
 {
-	if(argc != 3)
+	if(argc < 3)
 	{
-		std::cout << "Syntax: imgstats <fitsfile> <trunc value>\n";
+		std::cout << "Syntax: imgstats [-test <rmslimit>] <fitsfile> <trunc value>\n";
 	}
 	else {
-		FitsReader reader(argv[1]);
-		const double truncValue = atof(argv[2]);
+		size_t argi = 1;
+		bool hasLimit = false;
+		double limitRMS = 0.0;
+		
+		while(argv[argi][0] == '-')
+		{
+			std::string p = &argv[argi][1];
+			if(p == "test")
+			{
+				hasLimit = true;
+				++argi;
+				limitRMS = atof(argv[argi]);
+			}
+			else throw std::runtime_error("Unknown parameter");
+			++argi;
+		}
+		
+		FitsReader reader(argv[argi]);
+		const double truncValue = atof(argv[argi+1]);
 		
 		const size_t width = reader.ImageWidth(), height = reader.ImageHeight();
 		ao::uvector<double> image(width * height);
 		reader.Read(image.data());
 		
-		size_t boxSize = 20;
-		while(boxSize < width && boxSize < height)
+		if(hasLimit)
 		{
-			ao::uvector<double> samplesTruncated, samplesAll;
+			size_t boxSize = 160;
+			ao::uvector<double> samplesAll;
 			for(size_t boxY=0; boxY!=boxSize; ++boxY)
 			{
 				double* ptr = &image[(boxY + (height - boxSize)/2) * width + (width + boxSize)/2];
 				for(size_t boxX=0; boxX!=boxSize; ++boxX)
 				{
 					samplesAll.push_back(*ptr);
-					if(*ptr <= truncValue)
-						samplesTruncated.push_back(*ptr);
 					++ptr;
 				}
 			}
-			std::cout << "Box size " << boxSize << '\n';
-			std::cout << "All:       ";
-			reportValues(samplesAll);
-			std::cout << "Truncated: ";
-			reportValues(samplesTruncated);
-			boxSize *= 2;
+			reportValues(samplesAll, true, limitRMS);
+		}
+		else {
+			size_t boxSize = 20;
+			while(boxSize < width && boxSize < height)
+			{
+				ao::uvector<double> samplesTruncated, samplesAll;
+				for(size_t boxY=0; boxY!=boxSize; ++boxY)
+				{
+					double* ptr = &image[(boxY + (height - boxSize)/2) * width + (width + boxSize)/2];
+					for(size_t boxX=0; boxX!=boxSize; ++boxX)
+					{
+						samplesAll.push_back(*ptr);
+						if(*ptr <= truncValue)
+							samplesTruncated.push_back(*ptr);
+						++ptr;
+					}
+				}
+				std::cout << "Box size " << boxSize << '\n';
+				std::cout << "All:       ";
+				reportValues(samplesAll);
+				std::cout << "Truncated: ";
+				reportValues(samplesTruncated);
+				boxSize *= 2;
+			}
 		}
 	}
 }
